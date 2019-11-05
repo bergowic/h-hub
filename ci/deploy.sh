eef6ba9d2c2ef9638152420c4022479243a4ec61
bergic <ljberg@gmx.de>
2019-11-05 20:36:53 +0100

    build app

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -8,5 +8,7 @@ cd ../league-parser
 sls deploy
 cd ../league-parser-trigger
 sls deploy
+cd ../scorers
+npm run build
 aws s3 rm s3://$AWS_S3_BUCKET --recursive --exclude assets/*
-aws s3 cp scorers/build s3://$AWS_S3_BUCKET --acl public-read --recursive --exclude assets/*
+aws s3 cp build s3://$AWS_S3_BUCKET --recursive --exclude assets/* --acl public-read

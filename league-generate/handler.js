cdeea551d2904affe7b62f1131dd85d1730e245d
bergic <ljberg@gmx.de>
2019-11-07 16:43:56 +0100

    log games when generating

--- league-generate/handler.js
+++ league-generate/handler.js
@@ -24,6 +24,8 @@ module.exports.generateLeague = (event, context, cb) => {
 		} else {
 			const games = data.Items.map((game) => AWS.DynamoDB.Converter.unmarshall(game));
 
+			games.forEach(game => console.log(game));
+
 			s3.putObject({
 				Bucket: process.env.BUCKET_NAME,
 				Key: 'assets/league-' + league._id + '.json',

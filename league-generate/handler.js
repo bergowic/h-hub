40c5006f08e0faaf78d7a61ea820314082c24b35
bergic <ljberg@gmx.de>
2019-11-09 03:24:39 +0100

    read all items on scan

--- league-generate/handler.js
+++ league-generate/handler.js
@@ -5,6 +5,10 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB();
 const s3 = new AWS.S3();
 
+function unmarshallGames(games) {
+	return games.map((game) => AWS.DynamoDB.Converter.unmarshall(game));
+}
+
 module.exports.generateLeague = (event, context, cb) => {
 	const league = JSON.parse(event.Records[0].body);
 
@@ -18,22 +22,28 @@ module.exports.generateLeague = (event, context, cb) => {
 		FilterExpression: 'leagueId = :leagueId',
 	};
 
-	dynamoDb.scan(params, (err, data) => {
+	let games = [];
+
+	onScan(err, data) {
 		if (err) {
 			cb(err);
 		} else {
-			console.log('LastEvaluatedKey', data.LastEvaluatedKey);
-			const games = data.Items.map((game) => AWS.DynamoDB.Converter.unmarshall(game));
+			games = games.concat(unmarshallGames(data.Items));
 
-			games.forEach((game, i) => console.log(i, JSON.stringify(game)));
-
-			s3.putObject({
-				Bucket: process.env.BUCKET_NAME,
-				Key: 'assets/league-' + league._id + '.json',
-				Body: JSON.stringify(games),
-				ContentType: 'application/json',
-				CacheControl: 'no-cache',
-			}, cb);
+			if (data.LastEvaluatedKey) {
+				params.ExclusiveStartKey = data.LastEvaluatedKey;
+				dynamoDb.scan(params, onScan;
+			} else {
+				s3.putObject({
+					Bucket: process.env.BUCKET_NAME,
+					Key: 'assets/league-' + league._id + '.json',
+					Body: JSON.stringify(games),
+					ContentType: 'application/json',
+					CacheControl: 'no-cache',
+				}, cb);
+			}
 		}
-	});
+	}
+
+	dynamoDb.scan(params, onScan;
 };

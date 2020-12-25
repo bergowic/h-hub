998f73804d6f54717638c003bd5388f67e3cf780
bergic <ljberg@gmx.de>
2020-12-25 13:30:51 +0100

    Set cdk dnv

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new CdkStack(app, 'CdkStack', {
+    env: { 
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION || 'eu-central-1',
+    }
+});

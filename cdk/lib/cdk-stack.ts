4c85cceac86942f705da6b104ab93d89e6725ae6
bergic <ljberg@gmx.de>
2020-12-25 13:54:53 +0100

    Wrong scope

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -5,7 +5,7 @@ export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new lambda.Function(scope, 'AssociationParser', {
+    new lambda.Function(this, 'AssociationParser', {
       code: lambda.Code.fromAsset('dist/association-parser'),
       handler: 'main.handler',
       runtime: lambda.Runtime.NODEJS_12_X,

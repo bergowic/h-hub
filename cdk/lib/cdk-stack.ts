aab8eb462b4141e05880fd7d85a906c8b054703b
bergic <ljberg@gmx.de>
2020-12-25 13:51:47 +0100

    Deploy association parser as lambda

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,7 +1,14 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new lambda.Function(scope, 'AssociationParser', {
+      code: lambda.Code.fromAsset('dist/association-parser'),
+      handler: 'main.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+    })
   }
 }

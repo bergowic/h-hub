39a6b9d59d3c680355359100b7948a43ae885db6
bergowic <ljberg@gmx.de>
2020-12-25 20:57:47 +0100

    Rename CLOUD to STAGE

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,13 +3,13 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkStack } from '../lib/cdk-stack';
 
-const CLOUD = process.env.CLOUD
-if (!CLOUD) {
-    throw new Error("CLOUD is not defined")
+const STAGE = process.env.STAGE
+if (!STAGE) {
+    throw new Error("STAGE is not defined")
 }
 
 const app = new cdk.App();
-new CdkStack(app, `${CLOUD}-h-hub`, {
+new CdkStack(app, `${STAGE}-h-hub`, {
     env: { 
         account: process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION || 'eu-central-1',
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-(export CLOUD=local && cd cdk && cdk deploy)
+(export STAGE=local && cd cdk && cdk deploy)

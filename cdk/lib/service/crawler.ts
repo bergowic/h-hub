d22b28bcdd41152eaa41013cfbea8675ac789915
bergowic <ljberg@gmx.de>
2021-01-29 08:51:30 +0100

    Add merger to sfn

--- cdk/lib/service/crawler.ts
+++ cdk/lib/service/crawler.ts
@@ -10,9 +10,14 @@ interface Task {
     description: string,
 }
 
-const BASE_ORGANISATION_PARSER = {
+const ORGANISATION_PARSER = {
     app: "organisation-parser",
     description: "Parse base organisation",
+}
+
+const ORGANISATION_MERGER = {
+    app: "organisation-merger",
+    description: "Merge organisations",
 }
 
 const lambdas: {[key: string]: lambda.Function} = {}
@@ -34,14 +39,21 @@ function getInvoke(scope: CdkStack, task: Task) {
 }   
 
 function parseOrganisation(scope: CdkStack) {
-    const parser = getInvoke(scope, BASE_ORGANISATION_PARSER)
+    const parser = getInvoke(scope, ORGANISATION_PARSER)
 
     return map(scope, "Parse base organisations")
         .iterator(parser)
 }
 
+function mergeOrganisations(scope: CdkStack) {
+    const merger = getInvoke(scope, ORGANISATION_MERGER)
+
+    return merger
+}
+
 export function createCrawler(scope: CdkStack) {
     return run(scope, "GamesCrawler", {
         definition: parseOrganisation(scope)
+            .next(mergeOrganisations(scope))
     })
 }

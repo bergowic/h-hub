9a1aebcea6eac69d7fc6baa4c3a807cae14ec8a8
bergowic <ljberg@gmx.de>
2024-01-01 18:20:38 +0100

    Fail pipeline if UI cannot be built

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,6 @@
 cd data-resolver
 sls deploy --stage prod || exit 1
 cd ../scorers
-npm run build
+npm run build || exit 1
 aws s3 rm s3://$AWS_S3_BUCKET --recursive --exclude assets/*
 aws s3 cp build s3://$AWS_S3_BUCKET --recursive --exclude assets/* --acl public-read
